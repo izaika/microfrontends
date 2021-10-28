18d90343d095db4119cd386b69abd5c8b910bd9e
Igor Zaika <izaika89@icloud.com>
2021-10-28 22:00:02 +0300

    lazy load

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Header from './components/Header';
-import AuthApp from './components/AuthApp';
-import MarketingApp from './components/MarketingApp';
+import Progress from './components/Progress';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
-})
+});
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 export default () => {
   return (
@@ -16,10 +18,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
